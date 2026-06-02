c80b1178cfb3309c94f08164758b42777d719459
pt-main <pt.main.acc@gmail.com>
2026-06-02 18:40:45 +0300

    Some fixes

--- events/engine.go
+++ events/engine.go
@@ -29,12 +29,15 @@ func (de *DefaultEvents) CallEvent(e *system.Engine) error {
 	_parsed, _ := e.Scope["parsed_[]ParsedNode"]
 	parsed, ok := _parsed.([]parsing.ParsedNode)
 	if !ok {
-		return errors.New("Cant start call event. Invalid type of parsed result.")
+		return errors.New("Can't start call event. Invalid type of parsed result.")
 	}
 	for _, node := range parsed {
 		cmd_switch := node.Switch
-		handler := e.Commands[cmd_switch].Handler
-		err := handler(e, node)
+		handler, ok := e.Commands[cmd_switch]
+		err := errors.New("")
+		if ok {
+			err = handler.Handler(e, node)
+		}
 		_raw, ok := node.Metadata["__raw"]
 		if !ok {
 			return errors.New("Can't get raw string of code")
