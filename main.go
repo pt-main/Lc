98f368174e57b61361047d42a26a00064d5e1c2a
pt-main <pt.main.acc@gmail.com>
2026-06-14 15:32:08 +0300

    Update 0.10.1

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/pt-main/lc/stringParsing"
 )
 
-const Version = "0.9.15"
+const Version = "0.10.1"
 
 // NewStringEngine creates a ready-to-use string-based engine.
 // Parameters:
